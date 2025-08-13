33a481a42aa30048049b36c42a7fc41c67b0c7b4
Gautam Singh Kunwar <gautamkunwar05@gmail.com>
2025-08-13 09:18:26 +0530

    Added new features

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import { useState } from "react"
+import Navbar from "./components/Navbar"
+import {Route, Routes, useLocation } from "react-router-dom"
+import Home from "./pages/Home"
+import CarDetail from "./pages/CarDetail"
+import Cars from "./pages/Cars"
+import MyBookings from "./pages/MyBookings"
 
 const App = () => {
+  const [showLogin, setShowLogin] = useState(false)
+  const isAdminPath = useLocation().pathname.startsWith('/admin')
   return (
-    <div>
-      App
-      </div>
+    <>
+      {!isAdminPath && <Navbar setShowLogin={setShowLogin}/>}
+
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/car-details/:id" element={<CarDetail/>}/>
+        <Route path="/cars" element={<Cars/>}/>
+        <Route path="/my-bookings" element={<MyBookings/>}/>
+      </Routes>
+      </>
   )
 }
 
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import FeaturedSection from "../components/FeaturedSection"
+import Hero from "../components/Hero"
+
+const Home = () => {
+    return (
+        <>
+            <Hero/>
+            <FeaturedSection/>
+        </>
+    )
+}
+
+export default Home
