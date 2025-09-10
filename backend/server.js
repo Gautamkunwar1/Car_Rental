73cca989f3facc5543f994c841f6bfac793fe2ae
Gautam Singh Kunwar <gautamkunwar05@gmail.com>
2025-09-10 12:27:44 +0530

    fixes bug

--- backend/server.js
+++ backend/server.js
@@ -10,17 +10,21 @@ dotenv.config();
 const app = express()
 const PORT = process.env.PORT || 3000;
 // Middlewares
-app.use(cors());
+app.use(cors({
+    origin: ["https://car-rental-lyart-seven.vercel.app/"],
+    credentials: true
+}));
+
 app.use(express.json());
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("Server is running")
 });
-app.use('/api/user',userRouter);
-app.use('/api/admin',adminRouter);
-app.use('/api/bookings',bookingRouter);
+app.use('/api/user', userRouter);
+app.use('/api/admin', adminRouter);
+app.use('/api/bookings', bookingRouter);
 
-app.listen(PORT,async()=>{
+app.listen(PORT, async () => {
     try {
         console.log(`Server is running at ${PORT}`)
         await connectDb()
